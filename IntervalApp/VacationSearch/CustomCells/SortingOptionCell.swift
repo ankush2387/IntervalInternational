d44dd8099ebb7b40bd18deae6003772fa2465e9c
Chetu <gandhij@chetu.com>
2017-06-20 17:05:21 +0530

    MOBI-352 : Soring By Options - changes in vacation search iPhone storyboard - Added new class for sorting view controller - Added custom cell for showing sorting option

---
+++ IntervalApp/VacationSearch/CustomCells/SortingOptionCell.swift
@@ -0,0 +1,28 @@
+//
+//  SortingOptionCell.swift
+//  IntervalApp
+//
+//  Created by Chetu on 20/06/17.
+//  Copyright © 2017 Interval International. All rights reserved.
+//
+
+import UIKit
+
+class SortingOptionCell: UITableViewCell {
+
+    
+    //Outlets
+    @IBOutlet weak var sortingOptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
