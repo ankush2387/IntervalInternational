647c133bae554b34164af069ebe076a371dd58b6
c_athapliyal <Abhilasha.Thapliyal@iilg.com>
2017-07-31 22:28:38 +0530

    Changes for sorting

---
+++ IntervalApp/VacationSearch/CustomCells/FilterCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterCell.swift
+//  IntervalApp
+//
+//  Created by Chetu on 31/07/17.
+//  Copyright © 2017 Interval International. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
