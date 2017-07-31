b7761880b188fc6f16433611522c6f4ac91455ec
c_athapliyal <Abhilasha.Thapliyal@iilg.com>
2017-07-31 14:35:49 +0530

    MOB:922:-Update iOS App to absorb the Address Schema changes in API endpoints. - Make UI changes in SortingViewController and vacationSearchIphoneStoryboard. - change cell height of exchange fees.

--- IntervalApp/VacationSearch/SortingViewController.swift
+++ IntervalApp/VacationSearch/SortingViewController.swift
@@ -22,28 +22,56 @@ class SortingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //remove extra separator of tableview
+        self.sortingTBLview.tableFooterView = UIView()
         
         self.title = Constant.ControllerTitles.sorting
         //***** Add the cancel button  as left bar button item *****//
         
         let cancelButton = UIBarButtonItem(title: Constant.buttonTitles.cancel, style: .plain, target: self, action: #selector(cancelButtonPressed(_:)))
 
-        cancelButton.tintColor = UIColor.white
-        self.navigationItem.leftBarButtonItem = cancelButton
+        cancelButton.tintColor = UIColor.init(colorLiteralRed: 52.0/255.0, green: 152.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+        self.navigationItem.rightBarButtonItem = cancelButton
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        
+        //self.navigationItem.leftBarButtonItem = cancelButton
         
         //***** Creating and adding right bar button for more option button *****//
-        let doneButton = UIBarButtonItem(title:Constant.AlertPromtMessages.done, style: .plain, target: self, action: #selector(doneButtonPressed(_:)))
-        doneButton.tintColor = UIColor.white
-        self.navigationItem.rightBarButtonItem = doneButton
+        /*let doneButton = UIBarButtonItem(title:Constant.AlertPromtMessages.done, style: .plain, target: self, action: #selector(doneButtonPressed(_:)))
+        
+        doneButton.tintColor = UIColor.white*/
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-    func doneButtonPressed(_ sender:UIBarButtonItem) {
+    /*func doneButtonPressed(_ sender:UIBarButtonItem) {
         
         self.navigationController?.dismiss(animated: true, completion: nil)
+    }*/
+    
+    @IBAction func checkBoxClicked(_ sender: IUIKCheckbox) {
+        let cell = sender.superview?.superview?.superview as? SortingOptionCell
+        
+        let indexPath = self.sortingTBLview.indexPath(for: cell!)
+        
+        self.selectedIndex = (indexPath?.row)!
+        
+        /*if cell?.checkBox.isSelected == true {
+            cell?.checkBox.checked = false
+            cell?.sortingOptionLabel.textColor = UIColor.lightGray
+            
+        } else {
+            cell?.sortingOptionLabel.textColor = IUIKColorPalette.secondaryB.color
+            cell?.checkBox.checked = true
+            
+        }*/
+        
+        self.sortingTBLview.reloadData()
+        
     }
     
     func cancelButtonPressed(_ sender:UIBarButtonItem) {
@@ -59,6 +87,7 @@ extension SortingViewController:UITableViewDelegate {
         
         self.selectedIndex = indexPath.row
         self.sortingTBLview.reloadData()
+        //self.navigationController?.dismiss(animated: true, completion: nil)
     }
     
 }
@@ -79,17 +108,17 @@ extension SortingViewController:UITableViewDataSource {
         if(self.selectedIndex == indexPath.row) {
             
             cell.sortingOptionLabel.textColor = IUIKColorPalette.secondaryB.color
+            cell.checkBox.checked = true
         }else {
             
             cell.sortingOptionLabel.textColor = UIColor.lightGray
+            cell.checkBox.checked = false
         }
         
         cell.selectionStyle = UITableViewCellSelectionStyle.none
         return cell
 
     }
-    
-        
     
     
 }
