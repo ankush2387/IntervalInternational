1128326cf8a0c0eb9133f347c0662ba9046e2c0f
Chetu <gandhij@chetu.com>
2017-08-09 14:36:26 +0530

    RentalInventoryCVCell added

---
+++ IntervalApp/VacationSearch/CustomCells/RentalInventoryCVCell.swift
@@ -0,0 +1,21 @@
+//
+//  RentalInventoryCVCell.swift
+//  IntervalApp
+//
+//  Created by Chetu on 09/08/17.
+//  Copyright © 2017 Interval International. All rights reserved.
+//
+
+import UIKit
+
+class RentalInventoryCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var getawayNameLabel: UILabel!
+    @IBOutlet weak var getawayPrice: UILabel!
+    @IBOutlet weak var bedRoomType: UILabel!
+    @IBOutlet weak var sleeps: UILabel!
+    @IBOutlet weak var kitchenType: UILabel!
+    @IBOutlet weak var promotionsView: UIView!
+    @IBOutlet weak var currencySymbol: UILabel!
+    
+}
