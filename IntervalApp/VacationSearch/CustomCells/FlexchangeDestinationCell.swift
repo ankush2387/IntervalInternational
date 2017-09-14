dc6c9a4e01b58f47dcd5c7abc7a9edfc6fbad1b0
Sonu Raj <sonur@chetu.com>
2017-09-14 14:42:34 +0530

    flexchange cell created

---
+++ IntervalApp/VacationSearch/CustomCells/FlexchangeDestinationCell.swift
@@ -0,0 +1,27 @@
+//
+//  FlexchangeDestinationCell.swift
+//  IntervalApp
+//
+//  Created by CHETUMAC043 on 9/13/17.
+//  Copyright © 2017 Interval International. All rights reserved.
+//
+
+import UIKit
+
+class FlexchangeDestinationCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var lblFlexchangeDestination: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
