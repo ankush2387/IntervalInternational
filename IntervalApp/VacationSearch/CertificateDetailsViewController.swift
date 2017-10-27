158c9c2fbb73a2b29aac07e8f03454cbe26ed75e
Ganhdi Jee <gandhij@chetu.com>
2017-10-27 22:48:14 +0530

    MOBI-1204 certificate details cell created

--- IntervalApp/VacationSearch/CertificateDetailsViewController.swift
+++ IntervalApp/VacationSearch/CertificateDetailsViewController.swift
@@ -52,15 +52,10 @@ extension CertificateDetailsViewController:UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-<<<<<<< HEAD
         var cell = UITableViewCell()
         cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: Constant.certificateScreenReusableIdentifiers.certificateDetailsCell)
         
         let response = certificateDetailsResponse
-=======
-        
-        let response = Constant.MyClassConstants.certificateDetailsArray
->>>>>>> MOBI-1214 navigation bar bug fixed in ipad
         let newLine = "\n"
         let newLine2 = "\n\n"
         
@@ -68,7 +63,6 @@ extension CertificateDetailsViewController:UITableViewDataSource {
         
         let footer = response.footer.joined(separator: newLine)
         
-<<<<<<< HEAD
         //restricted area
         var areaLbl = ""
         if let label = response.restrictedArea?.label {
@@ -98,41 +92,6 @@ extension CertificateDetailsViewController:UITableViewDataSource {
         cell.detailTextLabel?.numberOfLines = 0
         cell.detailTextLabel?.text = finaStr
         cell.detailTextLabel?.font = UIFont(name: Constant.fontName.helveticaNeue, size: 15)
-=======
-        let areaLbl = response.restrictedArea?.label
-        let areasCombined = response.restrictedArea?.areas.map{$0.areaName} as! [String]
-        let areaString = areasCombined.joined(separator: newLine)
-        
-        //resort
-        let resortLbl = response.restrictedResort?.label
-        let resortCombined = response.restrictedResort?.resorts.map{$0.resortName} as! [String]
-        let resortString = resortCombined.joined(separator: newLine)
-        
-        let finaStr  = headerString + newLine2 + areaLbl! + areaString + newLine2 + resortLbl! + resortString + newLine2 + footer
-        
-       
-        //parse certificate details datacertificate
-       /* let certificateSummary = Constant.MyClassConstants.certificateDetailsArray
-        
-        let certificateSummary1 = [certificateSummary.header.map{$0},
-                                   certificateSummary.restrictedArea!.label!,
-                                   certificateSummary.restrictedArea!.areas.map{$0.areaName!},
-                                   certificateSummary.restrictedResort!.label!,
-                                   certificateSummary.restrictedResort!.resorts.map{$0.resortName!},
-                                   certificateSummary.footer.map{$0}] as [Any]
-        print(certificateSummary1)
-        
-        let strCertificateDetails = certificateSummary1.map {$0}
-        let newFmap = strCertificateDetails.map {$0}
-        
-        print(newFmap)
-         var newStr = ""
-        for str in newFmap{
-           // newStr = "\(str), (\n)"
-            newStr.append("\(str), \n")
-        }*/
-        cell.lblCertificateDetails.text = finaStr
->>>>>>> MOBI-1214 navigation bar bug fixed in ipad
         
         return cell
     }
