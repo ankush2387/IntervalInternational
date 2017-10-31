092b9fefddedbef8950340331409c806060ad2fc
c_athapliyal <Abhilasha.Thapliyal@intervalintl.com>
2017-10-31 19:36:06 +0530

    Changes for alert search result

--- IntervalApp/ResortDirectory/GoogleMapViewController.swift
+++ IntervalApp/ResortDirectory/GoogleMapViewController.swift
@@ -1370,7 +1370,9 @@ extension GoogleMapViewController:GMSMapViewDelegate {
                 
             }
         else {
-            if(Constant.MyClassConstants.isRunningOnIphone){
+            
+            if(!Constant.MyClassConstants.isRunningOnIphone){
+                
                 self.alertView.isHidden = false
                 self.mapTableView.isHidden = true
             }
