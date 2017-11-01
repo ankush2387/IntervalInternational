e357e3bde53068ee11363dab49a9c061dd036c68
c_athapliyal <Abhilasha.Thapliyal@intervalintl.com>
2017-11-01 18:50:51 +0530

    MOBI-1204:Modifications for top 10 search (iPad) - Includes deletion of unnecessary usage of self as per the comments - Includes catching the exceptions while using Realm using do catch - Includes removal of forced unwrapping with optional - Includes removal of commented code as well as added comments to functions.

--- IntervalApp/VacationSearch/VacationSearchViewController.swift
+++ IntervalApp/VacationSearch/VacationSearchViewController.swift
@@ -1297,6 +1297,10 @@ extension VacationSearchViewController:UITableViewDataSource {
                     self.presentErrorAlert(UserFacingCommonError.generic)
                 }
                 
+            }catch{
+                    
+                }
+                
             }
             else {
                 Constant.MyClassConstants.checkInClosestContentArray.removeObject(at: (indexPath as NSIndexPath).row)
@@ -1350,7 +1354,7 @@ extension VacationSearchViewController:WhoIsTravelingCellDelegate {
         //***** updating adult counter increment and decrement
         adultCounter = value
         if defaults.object(forKey: Constant.MyClassConstants.adultCounterString) != nil {
-            
+
             defaults.removeObject(forKey: Constant.MyClassConstants.adultCounterString)
             defaults.set(value, forKey: Constant.MyClassConstants.adultCounterString)
             defaults.synchronize()
