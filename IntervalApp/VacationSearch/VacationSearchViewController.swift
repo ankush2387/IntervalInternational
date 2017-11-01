f884ad0b45ba20218b1cfdd892116c25411f0d3a
c_athapliyal <Abhilasha.Thapliyal@intervalintl.com>
2017-11-01 22:55:13 +0530

    MOBI-1204:Modifications for top 10 search (iPad) - Removed forced optionals - Changes for do catch error handling

--- IntervalApp/VacationSearch/VacationSearchViewController.swift
+++ IntervalApp/VacationSearch/VacationSearchViewController.swift
@@ -558,7 +558,7 @@ extension VacationSearchViewController:UITableViewDelegate {
                                 try realm.write {
                                     realm.delete(self.destinationOrResort[(indexPath as NSIndexPath).row])
                                 }
-                            }catch{
+                            }catch let error{
                                 self.presentErrorAlert(UserFacingCommonError.generic)
                             }
                         }else {
@@ -574,10 +574,9 @@ extension VacationSearchViewController:UITableViewDelegate {
                             tableView.reloadSections(IndexSet(integer:(indexPath as NSIndexPath).section), with: .automatic)
                             Helper.InitializeArrayFromLocalStorage()
                         }
-                    }catch{
+                    }catch let error{
                         self.presentErrorAlert(UserFacingCommonError.generic)
                     }
-                }else {
                     Constant.MyClassConstants.checkInClosestContentArray.removeObject(at: (indexPath as NSIndexPath).row)
                     tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
                     let delayTime = DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
@@ -687,10 +686,10 @@ extension VacationSearchViewController:UITableViewDelegate {
                                 tableView.reloadSections(IndexSet(integer:(indexPath as NSIndexPath).section), with: .automatic)
                                 Helper.InitializeOpenWeeksFromLocalStorage()
                             }
-                        }catch{
+                        }catch let error{
                             self.presentErrorAlert(UserFacingCommonError.generic)
                         }
-                    }catch{
+                    }catch let error{
                         self.presentErrorAlert(UserFacingCommonError.generic)
                     }
                 }
@@ -1290,11 +1289,12 @@ extension VacationSearchViewController:UITableViewDataSource {
                         DispatchQueue.main.asyncAfter(deadline: delayTime) {
                             tableView.reloadSections(IndexSet(integer:(indexPath as NSIndexPath).section), with: .automatic)
                         }
-                    }catch{
-                        
+                    }catch let error{
+                        self.presentErrorAlert(UserFacingCommonError.generic)
                     }
                     
-                }catch{
+                }catch let error{
+                    self.presentErrorAlert(UserFacingCommonError.generic)
                     
                 }
                 
