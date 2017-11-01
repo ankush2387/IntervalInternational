ff9a104a61ff98b89f603528b3003aed1b5624d5
Aylwing Olivas <aylwing.olivas@intervalintl.com>
2017-11-01 15:35:23 -0400

    Moved comments outside function for easy access at call site.

--- IntervalApp/Common/Utils/Print+IntervalDebug.swift
+++ IntervalApp/Common/Utils/Print+IntervalDebug.swift
@@ -8,15 +8,15 @@
 
 import Foundation
 
+/// If the print to debug console is being called when built in debug mode - Print items
 func intervalPrint(_ items: Any..., separator: String = "", terminator: String = "") {
-    // If the print to debug console is being called when built in debug mode - Print items
     #if DEBUG
         print(items, separator: separator, terminator: terminator)
     #endif
 }
 
+/// If the debugPrint to debug console is being called when built in debug mode - Print items
 func intervalDebugPrint(_ items: Any..., separator: String = "", terminator: String = "") {
-    // If the debugPrint to debug console is being called when built in debug mode - Print items
     #if DEBUG
         debugPrint(items, separator: separator, terminator: terminator)
     #endif
