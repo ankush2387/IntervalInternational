c71b3bcfbb6455ad79b712879f5aa227cb877637
Gandhi Jee <gandhij@chetu.com>
2017-11-01 22:57:47 +0530

    MOBI-1218:Code optimisation for unwanted code and unused variables - changes in constant file file - changes in membership details table cell - changes in weather view controller

--- IntervalApp/Common/Utils/Constant.swift
+++ IntervalApp/Common/Utils/Constant.swift
@@ -1043,8 +1043,6 @@ class Constant: NSObject {
         static var renewalOtherOptionsVC = "RenewalOtherOptionsVC"
         static var certificateDetailsViewController = "CertificateDetailsViewController"
         
-        static var certificateDetailsViewController = "CertificateDetailsViewController"
-        
     }
     
     //***** common  structure to provide all custom nib cell names *****//
