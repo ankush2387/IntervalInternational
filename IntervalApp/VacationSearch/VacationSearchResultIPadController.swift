9d5a716d33a899ed1ca7fd01cbea6206ef8ed47e
c_athapliyal <Abhilasha.Thapliyal@intervalintl.com>
2017-11-02 18:26:40 +0530

    MOBI-1219:Remove unwanted code and unused variables - Includes removal of simple alert - Includes removal of unwanted code

--- IntervalApp/VacationSearch/VacationSearchResultIPadController.swift
+++ IntervalApp/VacationSearch/VacationSearchResultIPadController.swift
@@ -1590,7 +1590,6 @@ extension VacationSearchResultIPadController:UITableViewDataSource {
         
         let dropDownImgVw = UIImageView(frame: CGRect(x: self.resortDetailTBLView.frame.width - 40, y: 5, width: 30, height: 30))
         dropDownImgVw.image = UIImage(named: Constant.assetImageNames.dropArrow)
-
         if !Constant.MyClassConstants.noFilterOptions || alertFilterOptionsArray.count > 0 {
             if Constant.MyClassConstants.filterOptionsArray.count > 1 || alertFilterOptionsArray.count > 1 {
                 headerView.addSubview(dropDownImgVw)
