4f049254b749fb96a1509b9fda03a62eaf55de66
Ganhdi Jee <gandhij@chetu.com>
2017-11-02 17:05:56 +0530

    MOBI-1219:Remove unwanted code and unused variables

--- IntervalApp/Common/Utils/Constant.swift
+++ IntervalApp/Common/Utils/Constant.swift
@@ -617,8 +617,6 @@ class Constant: NSObject {
         static var certifcateCount = 0
         static var certificateArray = [AccommodationCertificate]()
         static var noFilterOptions = false
-        static var certificateDetailsArray = AccommodationCertificateSummary()
-        
     }
     
     // Enum to store resorts and destinations
