097ddfd60b73cb88b8ffdeb8d6a9b4e4fcba0489
Sandeep Tomar <sandeept@chetu.com>
2017-11-22 22:41:54 +0530

    MOBI-1271, Full Kitchen, Sleeps 0 Total, 0 private should not be displayed in Used to trade into

--- IntervalApp/VacationSearch/CustomCells/CertificateCell.swift
+++ IntervalApp/VacationSearch/CustomCells/CertificateCell.swift
@@ -11,40 +11,22 @@ import UIKit
 class CertificateCell: UITableViewCell {
     
     // Travel Window Outlets...
-    
     @IBOutlet weak var travelWindowStartDateLbl: UILabel!
-    
     @IBOutlet weak var travelWindowStartDayLbl: UILabel!
-    
     @IBOutlet weak var travelWindowStartMonthYearLbl: UILabel!
-    
     @IBOutlet weak var travelWindowEndDateLbl: UILabel!
-    
     @IBOutlet weak var travelWindowEndDayLbl: UILabel!
-    
     @IBOutlet weak var travelWindowEndMonthYearLbl: UILabel!
-    
     @IBOutlet weak var cellBaseView: UIView!
     @IBOutlet weak var certificateNumber: UILabel!
-    
     @IBOutlet weak var bedroomSize: UILabel!
-    @IBOutlet weak var totalSleeps: UILabel!
-    
     @IBOutlet weak var expireDate: UILabel!
-    
     @IBOutlet weak var statusLbl: UILabel!
-    
     @IBOutlet weak var certificateInfoButton: UIButton!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
         // Configure the view for the selected state
     }
-    
 }
