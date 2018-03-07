9aa50d974051bf8875c3f0d496544260a51b651d
Sandeep Tomar <sandeept@chetu.com>
2018-03-07 18:30:30 +0530

    MOBI-1706, Map View - Default Zoom Should Show Resort Markers

--- IntervalApp/ResortDirectory/GoogleMapViewController.swift
+++ IntervalApp/ResortDirectory/GoogleMapViewController.swift
@@ -403,8 +403,8 @@ class GoogleMapViewController: UIViewController {
         if needCameraChange {
             needCameraChange = false
             if let latitude = selectedDestination.geoArea?.southEastCoordinate?.latitude, let longitude = selectedDestination.geoArea?.northWestCoordinate?.longitude {
-                let targetDestination = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                mapView.camera = GMSCameraPosition.camera(withTarget: targetDestination, zoom: 10)
+                let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 10)
+                mapView.animate(to: camera)
             }
         }
         
@@ -420,7 +420,7 @@ class GoogleMapViewController: UIViewController {
         
         if needCameraChange {
             needCameraChange = false
-            let camera = GMSCameraPosition.camera(withLatitude: (Constant.MyClassConstants.resortsArray[0].coordinates?.latitude)!, longitude: (Constant.MyClassConstants.resortsArray[0].coordinates?.longitude)!, zoom: 10)
+            let camera = GMSCameraPosition.camera(withLatitude: (Constant.MyClassConstants.resortsArray[0].coordinates?.latitude)!, longitude: (Constant.MyClassConstants.resortsArray[0].coordinates?.longitude)!, zoom: 8.0)
            
             mapView.camera = camera
         }
